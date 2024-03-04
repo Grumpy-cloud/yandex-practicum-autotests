4fbad2b905a7beae645b807c0b962494d65843fc
Sergey <s.makarov@ispras.ru>
2024-03-04 23:36:14 +0300

    add request bodies

---
+++ src/data.py
@@ -0,0 +1,13 @@
+headers = {
+    'Content-Type': 'application/json',
+}
+
+user_body = {
+    'firstName': 'Сергей',
+    'phone': '',
+    'address': ''
+}
+
+kit_creation_body = {
+    'name': 'Мой набор'
+}
