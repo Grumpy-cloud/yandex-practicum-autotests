decab96caf53fd80c9b1048fa74498ff893336d9
Sergey <s.makarov@ispras.ru>
2024-03-04 23:35:55 +0300

    add paths

---
+++ src/configuration.py
@@ -0,0 +1,12 @@
+URL = ''
+
+# ======Пути запросов====== #
+# ===POST=== #
+CREATE_KIT = '/api/v1/kits'
+CREATE_USER = '/api/v1/users'
+# ========== #
+
+# ===DELETE=== #
+DELETE_KIT = '/api/v1/kits/'
+# ============ #
+# ========================= #
